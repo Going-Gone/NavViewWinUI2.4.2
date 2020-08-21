b163f8c32bdb4dd72f58287fbe7ffd7e23901765
Daren Sell <daren.sell.going@gmail.com>
2020-08-21 13:54:02 -0500

    Add project files.

---
+++ NavViewWinUI2.4.2/CustomMenuItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NavViewWinUI2._4._2
+{
+    public class CustomMenuItem
+    {
+        public string HeaderTitle { get; set; }
+    }
+}
---
+++ NavViewWinUI2.4.2/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NavViewWinUI2._4._2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private ObservableCollection<CustomMenuItem> Headers { get; set; }
+        public MainPage()
+        {
+            CreateHeaders();
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private void CreateHeaders()
+        {
+            Headers = new ObservableCollection<CustomMenuItem>();
+            Headers.Add(new CustomMenuItem { HeaderTitle = "Header" });
+            Headers.Add(new CustomMenuItem { HeaderTitle = "Header" });
+            Headers.Add(new CustomMenuItem { HeaderTitle = "Header" });
+            Headers.Add(new CustomMenuItem { HeaderTitle = "Header" });
+
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            nvMenu.UpdateLayout();
+            nvMenu.SelectedItem = Headers.FirstOrDefault();
+        }
+    }
+}
